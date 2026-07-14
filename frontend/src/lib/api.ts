5ad1ae28ec2bcf15c3aa12a7ceb42591f37efe93
hiroshi takato <hrsh.tkt@gmail.com>
2026-07-14 17:36:12 +0900

    update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -21,7 +21,7 @@ api.interceptors.response.use(
         const originalRequest = error.config;
 
         if (
-            (error.response?.status === 401 || error.response?.status === 403) &&
+            error.response?.status === 401 &&
             !originalRequest._retry &&
             !error.config?.url?.includes("/api/auth/refresh")
         ) {
